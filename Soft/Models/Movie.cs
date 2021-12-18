e6bf10676ca02e138fa995ff24587bb1fb004da4
Artur Valdna <arturvaldna@gmail.com>
2021-12-18 14:54:27 +0200

    Added base models for View: Movie and MoviesList

---
+++ Soft/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Soft.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
