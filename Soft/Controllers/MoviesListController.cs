b6b8faf48bb232cfd5a118430d66e965c3a787fb
Artur Valdna <arturvaldna@gmail.com>
2021-12-19 21:46:29 +0200

    Added master detail and a method for it, but some bugs exist.

--- Soft/Controllers/MoviesListController.cs
+++ Soft/Controllers/MoviesListController.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Soft.Models;
 using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
 
 namespace Soft.Controllers {
     public class MoviesListController : ApiController {
@@ -16,5 +15,11 @@ namespace Soft.Controllers {
             return Request.CreateResponse(DataSourceLoader.Load(MoviesList.Movies, loadOptions));
         }
 
+        [HttpGet]
+        public HttpResponseMessage GetMoviesDescriptions(int id, DataSourceLoadOptions loadOptions)
+        {
+            var description = MoviesList.Movies.Where(m => m.MovieId == id).Select(m => m.Description).FirstOrDefault();
+            return Request.CreateResponse(DataSourceLoader.Load(description, loadOptions));
+        }
     }
 }
