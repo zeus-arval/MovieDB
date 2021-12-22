931853e36aa49a134d10082ace65e7d7e109f876
Artur Valdna <arturvaldna@gmail.com>
2021-12-22 19:10:08 +0200

    Added base repository class

---
+++ Infra/Common/BaseRepo.cs
@@ -0,0 +1,31 @@
+﻿using Data.Common;
+using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Common
+{
+    public abstract class BaseRepo<TEntity, TData> : IBaseRepo<TEntity>
+        where TData : class, Data.Common.IBaseData, new()
+        where TEntity : class, Domain.Common.IBaseData, new()
+    {
+        protected internal readonly List<TData> dataList;
+        public BaseRepo(List<TData> list = null)
+        {
+            dataList = list;
+        }
+        public TEntity Get(int id)
+        {
+            if (id < 0) return null;
+            if (dataList is null) return null;
+            var dataObject = dataList?.FirstOrDefault(data => data?.Id == id);
+            return ToEntity(dataObject);
+        }
+
+        public List<TEntity> Get() => dataList.Select(ToEntity).ToList();
+        protected internal abstract TEntity ToEntity(TData d);
+    }
+}
---
+++ Infra/Common/IBaseRepo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Infra.Common
+{
+    public interface IBaseRepo<T>
+    {
+        T Get(int id);
+        List<T> Get();
+    }
+}
