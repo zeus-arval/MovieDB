3161e702e255f4d97a72dcfd30b8984c4a04b8ea
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 16:01:30 +0200

    Tools for copying data between DAL and BLL

---
+++ Domain/Common/Aids/Copier.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Common.Aids
+{
+    /// <summary>
+    /// Class for transferring the data between layers
+    /// </summary>
+    public static class Copier
+    {
+        public static TTo CopyMembers<TFrom, TTo>(TFrom from, TTo to)
+        {
+            if (from == null) return default;
+            if (to == null) return default;
+            foreach (var property in from.GetType().GetProperties())
+            {
+                var name = property.Name;
+                var p = to?.GetType().GetProperty(name);
+                if (p == null) continue;
+                var v = property.GetValue(from);
+                Safe.Run(() => p.SetValue(to, v));
+            }
+            return to;
+        }
+    }
+}
