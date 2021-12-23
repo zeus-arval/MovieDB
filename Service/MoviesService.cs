3fffda92b6f521390a13e83473dd33cf2a0d2414
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 18:29:06 +0200

    Added movie service

---
+++ Service/MoviesService.cs
@@ -0,0 +1,21 @@
+﻿using Domain;
+using Domain.Common.Aids;
+using Infra;
+using Service.Common;
+
+namespace Service
+{
+    public class MoviesService : BaseService<Movie, MovieDetails>
+    {
+        public MoviesService() : this(null) { }
+        public MoviesService(MoviesRepo moviesRepo) : base(moviesRepo) { }
+
+        public MovieDetails GetMovieDetailsById(int id)
+        {
+            if (Items is null) return null;
+            if (id < 0 || id > Items.Count) return null;
+            Movie movie = _repo.Get(id);
+            return Copier.CopyMembers(movie, new MovieDetails()) ?? new MovieDetails();
+        }
+    }
+}
