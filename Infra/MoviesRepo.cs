1e116a0609df25adca9a07e5360af5df88635b6f
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 16:27:25 +0200

    Added repository class for movies

---
+++ Infra/MoviesRepo.cs
@@ -0,0 +1,22 @@
+﻿using Data;
+using Domain;
+using Infra.Common;
+using System.Collections.Generic;
+
+namespace Infra
+{
+    public class MoviesRepo : BaseRepo<Movie, MovieDetails, MovieData>
+    {
+        private readonly List<CategoryData> _categories;
+        public MoviesRepo() : this(null) { }
+        public MoviesRepo(MoviesList movies) : base(movies?.movies) 
+        {
+            _categories = movies?.categories;
+        }
+
+        protected internal override Movie ToEntity(MovieData d)
+            => new Movie(d, _categories);
+        protected internal override MovieDetails DetailsToEntity(MovieData d)
+            => new MovieDetails(d);
+    }
+}
