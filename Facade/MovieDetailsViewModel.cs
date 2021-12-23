ea9e707b3d4e688bf27b166600bb9a679eb905e6
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 22:12:37 +0200

    Added view models

---
+++ Facade/MovieDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Facade.Common;
+
+namespace Facade
+{
+    public class MovieDetailsViewModel : BaseViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public int Rating { get; set; }
+    }
+}
---
+++ Facade/MovieViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Facade.Common;
+
+namespace Facade
+{
+    public class MovieViewModel : BaseViewModel
+    {
+        public string Title { get; set; }
+        public string CategoryName { get; set; }
+        public int Year { get; set; }
+        public int Rating { get; set; }
+    }
+}
