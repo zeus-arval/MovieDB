e9651211828024a84940f518a465da3ab1499103
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 16:25:34 +0200

    Addede domains for movies and its details

---
+++ Domain/Movie.cs
@@ -0,0 +1,21 @@
+﻿using Data;
+using Domain.Common;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain
+{
+    public class Movie : BaseEntity<MovieData>
+    {
+        public Movie() : this(null, null) { }
+        public Movie(MovieData movie, List<CategoryData> categories) : base(movie)
+        {
+            CategoryName = categories.FirstOrDefault(x => x?.Id == CategoryId).Name;
+        }
+        public string Title => Data?.Title ?? string.Empty;
+        public int Year => Data?.Year ?? int.MinValue;
+        public int Rating => Data?.Rating ?? int.MinValue;
+        private int CategoryId => Data?.CategoryId ?? int.MinValue;
+        public string CategoryName { get; }
+    }
+}
---
+++ Domain/MovieDetails.cs
@@ -0,0 +1,15 @@
+﻿using Data;
+using Domain.Common;
+
+namespace Domain
+{
+    public class MovieDetails : BaseEntity<MovieData>
+    {
+        public MovieDetails() : this(null) { }
+        public MovieDetails(MovieData movie) : base(movie) { }
+        public string Title => Data?.Title ?? string.Empty;
+        public string Description => Data?.Description ?? string.Empty;
+        public int Year => Data?.Year ?? int.MinValue;
+        public int Rating => Data?.Rating ?? int.MinValue;
+    }
+}
