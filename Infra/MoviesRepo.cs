10b811ea49ce12c840b5d967154ec443d30cb3ce
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 19:57:05 +0200

    Movies Repository's constructor is updated

--- Infra/MoviesRepo.cs
+++ Infra/MoviesRepo.cs
@@ -8,10 +8,9 @@ namespace Infra
     public class MoviesRepo : BaseRepo<Movie, MovieData>
     {
         private readonly List<CategoryData> _categories;
-        public MoviesRepo() : this(null) { }
-        public MoviesRepo(MoviesList movies) : base(movies?.movies) 
+        public MoviesRepo() : base(MoviesList.movies)
         {
-            _categories = movies?.categories;
+            _categories = MoviesList.categories;
         }
 
         protected internal override Movie ToEntity(MovieData d)
