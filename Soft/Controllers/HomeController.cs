8262d82bf09c193cacb634bd72e933f0a751349c
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 22:16:25 +0200

    Controlllers are refactored and modified

--- Soft/Controllers/HomeController.cs
+++ Soft/Controllers/HomeController.cs
@@ -1,12 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace Soft.Controllers {
-    public class HomeController : Controller {
-        public ActionResult Index() {
+namespace Soft.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
             return View();
         }
     }
--- Soft/Controllers/MoviesListController.cs
+++ Soft/Controllers/MoviesListController.cs
@@ -1,25 +1,28 @@
-using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
-using Soft.Models;
 using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
-using System;
 using System.Collections.Generic;
+using Domain;
+using Service;
+using Facade;
 
 namespace Soft.Controllers {
     public class MoviesListController : ApiController {
+        private MoviesService _service = new MoviesService();
 
         [HttpGet]
         public HttpResponseMessage Get(DataSourceLoadOptions loadOptions) {
-            return Request.CreateResponse(DataSourceLoader.Load(MoviesList.Movies, loadOptions));
+            List<MovieViewModel> movies = _service.GetList();
+            return Request.CreateResponse(DataSourceLoader.Load(movies, loadOptions));
         }
 
         [HttpGet]
         public HttpResponseMessage GetMoviesDescriptions(int id, DataSourceLoadOptions loadOptions)
         {
-            var description = MoviesList.Movies.Where(m => m.MovieId == id).Select(m => m.Description).FirstOrDefault();
-            return Request.CreateResponse(DataSourceLoader.Load(description, loadOptions));
+            MovieDetailsViewModel detail = _service.GetMovieDetailsById(id);
+            List<MovieDetailsViewModel> details = new List<MovieDetailsViewModel> { detail };
+            return Request.CreateResponse(DataSourceLoader.Load(details ,loadOptions));
         }
     }
 }
