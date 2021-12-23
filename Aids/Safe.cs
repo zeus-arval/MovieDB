f5f8872d1ba21a23803697b88603d2b08d831f2e
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 18:19:48 +0200

    Aids moved to another folder

---
+++ Aids/Safe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Domain.Common.Aids
+{
+    /// <summary>
+    /// Class for safely providing actions and functions
+    /// </summary>
+    public static class Safe
+    {
+        public static T Run<T>(Func<T> function, T valueOnException)
+        {
+            try
+            {
+                return function();
+            }
+            catch (Exception e)
+            {
+                LogException(e);
+                return valueOnException;
+            }
+        }
+        public static void Run(Action action)
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                LogException(e);
+                throw;
+            }
+        }
+        public static void LogException(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
