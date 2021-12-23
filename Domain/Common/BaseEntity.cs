f16682d214d3e8a4b217f3aad70a1496c381715e
Artur Valdna <arturvaldna@gmail.com>
2021-12-23 16:25:10 +0200

    Added base domains

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using Data.Common;
+using Domain.Common.Aids;
+
+namespace Domain.Common
+{
+    public abstract class BaseEntity<TData> : IBaseEntity
+        where TData : class, IBaseData, new()
+    {
+        protected readonly TData data;
+        public BaseEntity() : this(null) { }
+        public BaseEntity(TData d) => data = d;
+        public TData Data => Copier.CopyMembers(data, new TData()) ?? new TData();
+        public int Id => Data?.Id ?? int.MinValue;
+    }
+}
